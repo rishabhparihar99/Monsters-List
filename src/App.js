0752a3858a863e40356713f04920d32bb956f37e
Rishabh parihar <prishabh9@gmail.com>
2022-07-24 02:31:08 +0530

    Object added to name and fetch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     super();
 
     this.state = {
-      name: "Rishabh",
+      name: { firstName: "Rishabh", lastName: "Parihar" },
       company: "JBM",
     };
   }
@@ -18,7 +18,8 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi {this.state.name} works at {this.state.company}
+            Hi {this.state.name.firstName} {this.state.name.lastName} works at{" "}
+            {this.state.company}
           </p>
           <button
             onClick={() => {
