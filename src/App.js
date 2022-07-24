d522323632aa63d2c7a2760b9be4df3c6c03a428
Rishabh parihar <prishabh9@gmail.com>
2022-07-24 20:08:11 +0530

    monsters name print

--- src/App.js
+++ src/App.js
@@ -7,40 +7,26 @@ class App extends Component {
     super();
 
     this.state = {
-      name: { firstName: "Rishabh", lastName: "Parihar" },
-      company: "JBM",
+      monster1: {
+        name: "Linda",
+      },
+      monster2: {
+        name: "Roha",
+      },
+      monster3: {
+        name: "Rhino",
+      },
     };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name.firstName} {this.state.name.lastName} works at{" "}
-            {this.state.company}
-          </p>
-          <button
-            onClick={() => {
-              this.setState(
-                () => {
-                  return {
-                    name: { firstName: "Andrei", lastName: "Dosa" },
-                  };
-                },
-                () => {
-                  console.log(this.state);
-                }
-              );
-            }}
-          >
-            Change name
-          </button>
-        </header>
+        <h1>{this.state.monster1.name}</h1>
+        <h1>{this.state.monster2.name}</h1>
+        <h1>{this.state.monster3.name}</h1>
       </div>
     );
   }
 }
-
 export default App;
