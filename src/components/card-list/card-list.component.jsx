267341ab51b3fca774ccd5d220a11b31553aff7d
Rishabh parihar <prishabh9@gmail.com>
2022-07-26 15:43:56 +0530

    component creation

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class CardList extends Component{
+   render(){
+    return(
+        <div>
+            <p>Hello, I am the Card List</p>
+        </div>
+    )
+   }
+}
+export default CardList
