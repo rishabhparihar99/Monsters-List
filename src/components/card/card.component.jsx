53afcd718fc6064568a93ef1d29007c69d21f342
Rishabh parihar <prishabh9@gmail.com>
2022-07-28 19:49:08 +0530

    final commit

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,16 +3,16 @@ import './card.styles.css';
 
 class Card extends Component{
     render(){
-        
+
        const {email,id,name} = this.props.monster;
        return(
-        <div classname = "card-container" key={id}>
+        <div className = 'card-container' key={id}>
                     <img alt={`monster ${name}`} 
                     src={`https://robohash.org/${id}?set=set2`}/>
                     <h2>{name}</h2>
                     <p>{email}</p>
         </div>
        )
-    }
+       }
 }
 export default Card;
